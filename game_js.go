db9240d273932de8ce52a00bb8125b55b19f132e
kts of kettek <kts@kettek.net>
2023-02-14 03:36:16 -0800

    Remove debug line

--- game_js.go
+++ game_js.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"syscall/js"
 )
 
 func (g *Game) SystemInit() {
-	fmt.Println("JS")
 	hash := js.Global().Get("location").Get("hash")
 	if !hash.IsUndefined() {
 		m := hash.String()[1:]
